382ab5e91195cbbfa6e50be750aabcb7eebb3a01
Arti Shintre <aartishintre100@gmail.com>
2026-02-07 23:12:06 +0530

    tic-tac-toe

--- part11/class_object.js
+++ part11/class_object.js
@@ -15,3 +15,12 @@ console.log(fortuner.start());
 let punch = new ToyotaCar();
 punch.setBrand("Sieera");
 console.log(punch);
+
+
+const user = {
+    username : "Arti",
+    loginCount : 5,
+    signIn : true
+
+}
+console.log(user.username);
