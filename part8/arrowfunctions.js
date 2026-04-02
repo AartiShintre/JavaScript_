3c0aecf75264862218b0e79fef554c6a6d870efd
Arti Shintre <aartishintre100@gmail.com>
2026-04-02 21:41:36 +0530

    arrow functions

---
+++ part8/arrowfunctions.js
@@ -0,0 +1,10 @@
+const calYear = birthYear => 2027 - 2006;
+console.log(calYear());
+
+const yearUntilRetirment = (birthYear, firstName) =>{
+    const age = 2026 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement} years`;
+}
+console.log(yearUntilRetirment(2006,"Arti" ));
+
